7d0b3a5d97e283e337c8bc6ef3e336277d59b0b9
Ruofu Du <ruofu.du@skola.taby.se>
2023-05-28 21:16:54 +0200

    change

--- db_connection.php
+++ db_connection.php
@@ -13,7 +13,7 @@ $conn = new mysqli($dbHost, $dbUser, $dbPass, $dbName);
 
 // Check connection
 if ($conn->connect_error) {
-    
+
     die("Connection failed: " . $conn->connect_error);
     echo '<script>alert("Sorry. We are having technical issues. Please login later.");</script>';
     exit();
